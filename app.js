a108a5af60c1b4d393d0635e368a02e2c5fa215d
Samantha Bretous <sam@samanthabretous.com>
2016-08-10 15:26:45 -0400

    test merge

--- app.js
+++ app.js
@@ -20,16 +20,29 @@ Pokemon.prototype.learnAttackSkill = function (objAttachSkill) {
 }
 
 Pokemon.prototype.attack = function (skillKey, pokemon) {
-
+  this.magic.currentMagic = 50;
 }
 
 Pokemon.prototype.showStatus = function(){
-  console.log(this.health + this.magic)
+  console.log(this.name + " status")
+  console.log("health: " + this.health);
+  console.log("magic: " + this.magic.currentMagic);
 }
 
 Pokemon.prototype.getMagic = function() {
   //10% of max magic
   //current magic can never go above max magic
+  var addMagic = Math.round(this.magic.maxMagic * .10) 
+  var updatedMagic = 0;
+  console.log("current", this.magic.currentMagic)
+  console.log("max", this.magic.maxMagic)
+  if(this.magic.currentMagic < this.magic.maxMagic){
+    updatedMagic = this.magic.currentMagic + addMagic
+    if(updatedMagic > this.magic.maxMagic){
+      updatedMagic = this.magic.maxMagic;
+    }
+  }
+  console.log(updatedMagic)
 }
 
 function gameOver() {
@@ -40,9 +53,11 @@ function reset() {
 
 }
 
-// var pikachu = new Pokemon("pikachu", 120, 80);
-// var poisonSeed = new AttackSkill ("poison seed", 20, 20);
-// var lightning = new AttackSkill("lightning", 40, 30);
-// pikachu.learnAttackSkill(poisonSeed)
-// pikachu.learnAttackSkill(lightning)
-// console.log(pikachu.skills.lightning);
+var pikachu = new Pokemon("pikachu", 120, 80);
+var poisonSeed = new AttackSkill ("poison seed", 20, 20);
+var lightning = new AttackSkill("lightning", 40, 30);
+pikachu.learnAttackSkill(poisonSeed)
+pikachu.learnAttackSkill(lightning)
+//pikachu.attack()
+pikachu.getMagic()
+//pikachu.showStatus()
