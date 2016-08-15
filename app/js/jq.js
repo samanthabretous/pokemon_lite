b950ee63aaa96690583c5aa9d1205f8981176605
Joshua Fermin <Joshua@C4Qs-MacBook-Pro-2.local>
2016-08-15 16:43:02 -0400

    test active reload

--- app/js/jq.js
+++ app/js/jq.js
@@ -1,8 +1,14 @@
 // once the player choses its pokemon create an array with all available attacks. make player an object where we can store the pokemon he selected, all available skills current health current magic.
-player1 = Pikachu;
-$("#top-left").click(function(){
-	player1.skills.forEach(function(value, key){
-		console.log("value", value);
-		console.log("key", key);
-	})
-});
+var currentPlayer = "player1"
+var player1 = new Player(Pikachu);
+var displaying = "mainMenu";
+function attackDisplay() {
+	displaying = "attack"
+	console.log("it works")
+	$("#top-left").html("<h6 id=\"top-left\">"+player1.pokemonSkills[0]+"</h6>")
+	$("#top-right").html("<h6 id=\"top-right\">"+player1.pokemonSkills[1]+"</h6>");
+	$("#bottom-left").html("<h6 id=\"bottom-left\">"+player1.pokemonSkills[2]+"</h6>");
+	$("#top-right").html("<h6 id=\"bottom-right\">"+player1.pokemonSkills[3]+"</h6>");
+};
+player1.skillsToArray();
+$("#top-left").on('click', attackDisplay);
