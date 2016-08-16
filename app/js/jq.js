3306fb510a1d475a49a8b649d2006b84f0ae4db1
Joshua Fermin <Joshua@C4Qs-MacBook-Pro-2.local>
2016-08-16 16:17:45 -0400

    top left and tope right buttons are now clickable

--- app/js/jq.js
+++ app/js/jq.js
@@ -1,18 +1,131 @@
 // once the player choses its pokemon create an array with all available attacks. make player an object where we can store the pokemon he selected, all available skills current health current magic.
 
-var currentPlayer = "player1"
-var player1 = new Player(Pikachu);
-player1.skillsToArray();
+// keeps track of current player
+var currentPlayer = "player1";
+$(".mainOptions > h4").html(currentPlayer);
+// temp creation of players 1 and 2
+var Player1 = new Player(Pikachu);
+Player1.skillsToArray();
+var Player2 = new Player(Bulbasaur);
+Player2.skillsToArray();
+////////
+var something = Player2
+// keeps track of what is being displayed
 var displaying = "mainMenu";
-function attackDisplay() {
-	displaying = "attack"
-	console.log("it works")
-	$("#top-left").html(player1.pokemonSkills[0]);
-	$("#top-right").html(player1.pokemonSkills[1]);
-	$("#bottom-left").html(player1.pokemonSkills[2]);
-	$("#bottom-right").html(player1.pokemonSkills[3]);
+
+// ability to switch between players
+function switchPlayers(){
+	if (currentPlayer === "player1"){
+		currentPlayer = "player2";
+	}else {
+		currentPlayer = "player1";
+	}
+	$(".mainOptions > h4").html(currentPlayer);
 };
-$("#top-left").on('click', attackDisplay);
+
+// what to display during the attack click
+function attackDisplay(player) {
+	displaying = "attack"
+	$("#top-left").html(player.pokemonSkills[0]);
+	$("#top-right").html(player.pokemonSkills[1]);
+	$("#bottom-left").html(player.pokemonSkills[2]);
+	$("#bottom-right").html(player.pokemonSkills[3]);
+};
+
+function mainDisplay(player, otherPlayer , $clicker) {
+	if (displaying == "attack"){
+		if (currentPlayer == "player1"){
+			player.pokemon.attack($clicker.html(), otherPlayer.pokemon);
+		}else {
+			player.pokemon.attack($clicker.html(), Player1.pokemon);
+		}
+	}else if(displaying == "pack"){
+		player.pokemon.getMagic();
+	}
+	displaying = "mainMenu";
+	switchPlayers();
+	$("#top-left").html("FIGHT");
+	$("#top-right").html("PACK");
+	$("#bottom-left").html("RUN");
+	$("#bottom-right").html("COOL");
+};
+
+function packDisplay (player){
+	displaying = "pack";
+	$("#top-left").html("Raspberries");
+	$("#top-right").html("");
+	$("#bottom-left").html("");
+	$("#bottom-right").html("");
+};
+
+
+// switch cases for buttons
+$("#top-left").on('click', function(){
+	if(currentPlayer == "player1"){
+		var player = Player1;
+		var otherPlayer = Player2;
+	}else {
+		var player = Player2;
+		var otherPlayer = Player1;
+	}
+	switch (displaying){
+		case "mainMenu" :
+			attackDisplay(player);
+			break;
+		default:
+			mainDisplay(player, otherPlayer ,$("#top-left"));
+	};
+});
+
+
+$("#top-right").on('click', function(){
+	if(currentPlayer == "player1"){
+		var player = Player1;
+		var otherPlayer = Player2;
+	}else {
+		var player = Player2;
+		var otherPlayer = Player1;
+	}
+	switch (displaying){
+		case "mainMenu" :
+			packDisplay(player);
+			break;
+		default:
+			mainDisplay(player, otherPlayer, $("#top-right"));
+	};
+});
+$("#bottom-left").on('click', function(){
+	if(currentPlayer == "player1"){
+		var player = Player1;
+		var otherPlayer = Player2;
+	}else {
+		var player = Player2;
+		var otherPlayer = Player1;
+	}
+	switch (displaying){
+		case "mainMenu" :
+			mainDisplay(player, otherPlayer, $("#bottom-left"));
+			break;
+		default:
+			mainDisplay(player, otherPlayer, $("#bottom-left"));
+	};
+});
+$("#bottom-right").on('click', function(){
+	if(currentPlayer == "player1"){
+		var player = Player1;
+		var otherPlayer = Player2;
+	}else {
+		var player = Player2;
+		var otherPlayer = Player1;
+	}
+	switch (displaying){
+		case "mainMenu" :
+			mainDisplay(player, otherPlayer, $("#bottom-left"));
+			break;
+		default:
+			mainDisplay(player, otherPlayer, $("#bottom-right"));
+	};
+});
 
 //switch 
 $('.startBattle').click(function (){
